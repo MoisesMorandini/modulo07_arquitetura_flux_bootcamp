c04948c0fe7a2363f005e26d2959ea7467ecb7d7
MoisesMorandini <moisesmorandini@moisesmorandini.local>
2019-10-07 19:53:54 -0300

    finish commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-
+import { ToastContainer } from 'react-toastify';
 import './config/ReactotronConfig';
 
 import GlobalStyle from './styles/global';
 import Routes from './routes';
 import Header from './components/Header';
+
+import history from './services/history';
 import store from './store';
 function App() {
   return (
     <Provider store={store}>
       {/* deixar disponivel o nosso store da aplicacao, disponivel para todos os componentes*/}
-      <BrowserRouter>
+      <Router history={history}>
         <Header />
         <Routes />
         <GlobalStyle />
-      </BrowserRouter>
+        <ToastContainer autoClose={3000} />
+      </Router>
     </Provider>
   );
 }
--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -1,11 +1,59 @@
-import { call, put, all, takeLatest } from 'redux-saga/effects';
-import { addToCartSucess } from './actions';
+import { call, select, put, all, takeLatest } from 'redux-saga/effects';
+import { addToCartSucess, updateAmountSucess } from './actions';
+import { formatPrice } from '../../../util/format';
 import api from '../../../services/api';
+import history from '../../../services/history';
 
+import { toast } from 'react-toastify';
 function* addToCart({ id }) {
-  const response = yield call(api.get, `/products/${id}`);
+  const productExists = yield select(state =>
+    state.cart.find(p => p.id === id)
+  );
+  //pega do api a quantidadade do stock
+  const stock = yield call(api.get, `/stock/${id}`);
+  //salve o stock
+  const stockAmount = stock.data.amount;
+  //veririca se o quanto de produto ja foi solicitado pro carrinho. Se existir, salva este valor
+  // else, inicia com 0
+  const currentAmount = productExists ? productExists.amount : 0;
 
-  yield put(addToCartSucess(response.data));
+  const amount = currentAmount + 1;
+
+  if (amount > stockAmount) {
+    toast.error('Quantidade solicitada fora do estoque');
+    return;
+  }
+
+  if (productExists) {
+    yield put(updateAmountSucess(id, amount));
+  } else {
+    const response = yield call(api.get, `/products/${id}`);
+
+    const data = {
+      ...response.data,
+      amount: 1,
+      priceFormatted: formatPrice(response.data.price),
+    };
+
+    yield put(addToCartSucess(data));
+    history.push('/cart');
+  }
 }
 
-export default all([takeLatest('@cart/ADD_REQUEST', addToCart)]);
+function* updateAmount({ id, amount }) {
+  if (amount <= 0) return;
+
+  const stock = yield call(api.get, `stock/${id}`);
+  const stockAmount = stock.data.amount;
+
+  if (amount > stockAmount) {
+    toast.error('Quantidade solicitada fora do estoque');
+    return;
+  }
+  yield put(updateAmountSucess(id, amount));
+}
+
+export default all([
+  takeLatest('@cart/ADD_REQUEST', addToCart),
+  takeLatest('@cart/UPDATE_AMOUNT_REQUEST', updateAmount),
+]);
